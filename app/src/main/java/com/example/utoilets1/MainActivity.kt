b89be9be6511460d73633590b293a0f54a682866
alexabarkley1 <alexabarkley1@gmail.com>
2022-02-20 10:39:19 -0600

    added recycler view

--- app/src/main/java/com/example/utoilets1/MainActivity.kt
+++ app/src/main/java/com/example/utoilets1/MainActivity.kt
@@ -6,6 +6,9 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.utoilets1.adapter.ItemAdapter
+import com.example.utoilets1.data.Datasource
 import android.view.Menu
 import android.view.MenuItem
 import com.example.utoilets1.ui.main.SectionsPagerAdapter
@@ -20,6 +23,15 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val myDataset = Datasource().loadBathrooms()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
